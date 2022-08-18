e259e4536e0959a8f1afa3cec589bc771c62a47d
Vladimir <vovchik.v.kf@gmail.com>
2022-08-18 20:24:04 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PlaylistSearcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string playlistPath;
+        private string destinationFolder = @"%UserProfile%\Desktop\Folder";
+        private int countFilesToCopy;
+        private string musicFilePathOld;
+        private string musicFilePathNew;
+        private bool stopToCopy = false;
+
+        List<string> musicFiles = new List<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+        private void LabelClose_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed) Close();
+        }
+
+        private void ButtonChoosePlaylist_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonDestinationFolder_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonStopCopy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Directory.CreateDirectory(destinationFolder);
+        }
+    }
+}
